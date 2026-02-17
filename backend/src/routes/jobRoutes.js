18acc9cd6d34fb75dcb94573b67045d32e0a4e78
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-02-17 22:38:53 +0530

    change in routes

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -1,14 +1,23 @@
-import { createJob, fetchAndStoreJobs, getJobs,fetchJobsFromAPI,getSavedJobs } from "../controllers/jobController.js";
-
-const router = express.Router();
+import {
+  createJob,
+  fetchAndStoreJobs,
+  getJobs,
+  fetchJobsFromAPI,
+  getSavedJobs,
+} from "../controllers/jobController.js";
 
 export default async function jobRoutes(fastify) {
+
+  // Create job manually
   fastify.post("/jobs", createJob);
+
+  // Get all jobs from DB
   fastify.get("/jobs", getJobs);
-  
 
+  // Fetch from Adzuna and store in DB
   fastify.get("/jobs/fetch", fetchAndStoreJobs);
-  router.get("/fetch", fetchJobsFromAPI);   
-router.get("/", getSavedJobs);  
-}
 
+  // Optional: if using separate controller names
+  fastify.get("/jobs/fetch-api", fetchJobsFromAPI);
+  fastify.get("/jobs/saved", getSavedJobs);
+}
