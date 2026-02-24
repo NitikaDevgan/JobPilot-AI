c79b63a1ec4c8699e39b8de1fca15bdb6e57cc8e
Nitika Devgan <nitikadevganisnd@gmail.com>
2026-02-24 22:41:25 +0530

    change in env

--- backend/src/services/adzunaService.js
+++ backend/src/services/adzunaService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import Job from "../models/Job.js";
 
 const ADZUNA_BASE_URL = "https://api.adzuna.com/v1/api/jobs";
-const COUNTRY = "in";
+const COUNTRY = process.env.ADZUNA_COUNTRY;
 
 const APP_ID = process.env.ADZUNA_APP_ID;
 const APP_KEY = process.env.ADZUNA_APP_KEY;
